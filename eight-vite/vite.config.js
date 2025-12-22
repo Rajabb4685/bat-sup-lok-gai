3385fdef473bf26af9f3851afae677d3594466cd
Jiana Sta Ana <jianastaana@mac.mynetworksettings.com>
2025-12-22 15:48:53 -0500

    changed vite

--- eight-vite/vite.config.js
+++ eight-vite/vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "/bat-sup-lok-gai/",
+  base: "/",   // ← THIS IS IMPORTANT FOR RENDER
   plugins: [react()],
 })
